075946cdb8dbe22dd47d61cf1fbee65d8a663268
Cargalher <cargalher@gmail.com>
2021-05-04 15:56:25 +0200

    condicion to calculate burguer + 1 topping

--- main.js
+++ main.js
@@ -12,13 +12,31 @@ var toppingsWithPrice = [["Cheese", 1.0], ["Egg", 1.5], ["Mustard", 0.5], ["Toma
 //to get the price of a topping "Mustard" we use
 //console.log(toppingsWithPrice[2][1]);
 
-functions calculateBurgerPrice (toppings){
-
-   if(toppings === "Cheese"){
-        
-    } else if (toppings === "Egg"){
-        
-    } else if (toppings === "Mustard"){
-
+function calculateHamburgerPrice (toppings) {
+    if (toppings === toppingsWithPrice[0][0]) {
+      hamburgerPrice += toppingsWithPrice[0][1];   
+      console.log(hamburgerPrice);     
+    } else if (toppings ===  toppingsWithPrice[1][0]) {
+      hamburgerPrice += toppingsWithPrice[1][1];   
+      console.log(hamburgerPrice);
+  
+    } else if (toppings === toppingsWithPrice[2][0]) {
+      hamburgerPrice += toppingsWithPrice[2][1];   
+      console.log(hamburgerPrice);
+  
+    } else if (toppings ===  toppingsWithPrice[3][0]) {
+      hamburgerPrice += toppingsWithPrice[3][1];   
+      console.log(hamburgerPrice);
+  
+    } else if (toppings ===  toppingsWithPrice[4][0]) {
+      hamburgerPrice += toppingsWithPrice[4][1];   
+      console.log(hamburgerPrice);
+  
+    } else if (toppings ===  toppingsWithPrice[5][0]) {
+      hamburgerPrice += toppingsWithPrice[5][1];   
+      console.log(hamburgerPrice);
+  
     }
-}
+  }
+  
+  calculateHamburgerPrice(toppingsWithPrice[5][0]);
