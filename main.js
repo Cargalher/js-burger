6d43edd58bfc504047c0da53c2588649dca17ee6
Cargalher <cargalher@gmail.com>
2021-05-04 15:27:30 +0200

    var burger and var topping prices

--- main.js
+++ main.js
@@ -2,3 +2,12 @@
 //after he choose to add any topping in the menu
 //discounting a coupon to the price if the user insert one
 
+var hamburgerPrice = 9.50;
+
+var toppingsWithPrice = [["Cheese", 1.0], ["Egg", 1.5], ["Mustard", 0.5], ["Tomato", 1.0], ["Lettuce", 1.0], ["Ketchup", 0.5]];
+
+//to get the topping "Cheese" we use 
+//console.log(toppingsWithPrice[0][0]);
+
+//to get the price of a topping "Mustard" we use
+//console.log(toppingsWithPrice[2][1]);
