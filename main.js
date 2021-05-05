5cc24802798a02f7ac4d9cfbc1a97cd013dadf1f
Cargalher <cargalher@gmail.com>
2021-05-05 03:23:02 +0200

    fixed js

--- main.js
+++ main.js
@@ -2,130 +2,90 @@
 //after he choose to add any topping in the menu
 //discounting a coupon to the price if the user insert one
 
-// var hamburgerPrice = 9.50;
 
-// var toppingsWithPrice = [["Cheese", 1.0], ["Egg", 1.5], ["Mustard", 0.5], ["Tomato", 1.0], ["Lettuce", 1.0], ["Ketchup", 0.5]];
+//declaring a variable with the Hamburger Price
+var hamburgerPrice = 9.50;
+console.log(hamburgerPrice);
 
-//to get the topping "Cheese" we use 
-//console.log(toppingsWithPrice[0][0]);
+//Storing in an Array the Toppings and their relative prices, for this reason we use two dimansional Array[[]]
+// with index [i][0] we access a topping store in the array
+// with index [i][1] we access the price of a particular topping
+var toppingsWithPrice = [["Cheese", 1.0], ["Egg", 1.5], ["Mustard", 0.5], ["Tomato", 1.0], ["Lettuce", 1.0], ["Ketchup", 0.5]];
+console.log(toppingsWithPrice);
 
-//to get the price of a topping "Mustard" we use
-//console.log(toppingsWithPrice[2][1]);
-
-// function calculateHamburgerPrice (toppings) {
-//     if (toppings === toppingsWithPrice[0][0]) {
-//       hamburgerPrice += toppingsWithPrice[0][1];   
-//       console.log(hamburgerPrice);     
-//     } else if (toppings ===  toppingsWithPrice[1][0]) {
-//       hamburgerPrice += toppingsWithPrice[1][1];   
-//       console.log(hamburgerPrice);
-  
-//     } else if (toppings === toppingsWithPrice[2][0]) {
-//       hamburgerPrice += toppingsWithPrice[2][1];   
-//       console.log(hamburgerPrice);
-  
-//     } else if (toppings ===  toppingsWithPrice[3][0]) {
-//       hamburgerPrice += toppingsWithPrice[3][1];   
-//       console.log(hamburgerPrice);
-  
-//     } else if (toppings ===  toppingsWithPrice[4][0]) {
-//       hamburgerPrice += toppingsWithPrice[4][1];   
-//       console.log(hamburgerPrice);
-  
-//     } else if (toppings ===  toppingsWithPrice[5][0]) {
-//       hamburgerPrice += toppingsWithPrice[5][1];   
-//       console.log(hamburgerPrice);
-  
-//     }
-//   }
-  
-//calculateHamburgerPrice(toppingsWithPrice[5][0]);
-
-
-// document.getElementById("calc_btn").addEventListener("click", calculateHamburgerPrice(toppings));
-
-// // function Hello() {
-// //   console.log("Hello something is happening")
-// //   return "Hello something is happening";
-// // }
-
-
-//     function calculateHamburgerPrice (toppings) {
-//         if (toppings === toppingsWithPrice[0][0]) {
-//           hamburgerPrice += toppingsWithPrice[0][1];   
-//           console.log(hamburgerPrice);     
-//         } else if (toppings ===  toppingsWithPrice[1][0]) {
-//           hamburgerPrice += toppingsWithPrice[1][1];   
-//           console.log(hamburgerPrice);
-      
-//         } else if (toppings === toppingsWithPrice[2][0]) {
-//           hamburgerPrice += toppingsWithPrice[2][1];   
-//           console.log(hamburgerPrice);
-      
-//         } else if (toppings ===  toppingsWithPrice[3][0]) {
-//           hamburgerPrice += toppingsWithPrice[3][1];   
-//           console.log(hamburgerPrice);
-      
-//         } else if (toppings ===  toppingsWithPrice[4][0]) {
-//           hamburgerPrice += toppingsWithPrice[4][1];   
-//           console.log(hamburgerPrice);
-      
-//         } else if (toppings ===  toppingsWithPrice[5][0]) {
-//           hamburgerPrice += toppingsWithPrice[5][1];   
-//           console.log(hamburgerPrice);
-      
-//         }
-//         return hamburgerPrice;
-//         console.log(hamburgerPrice);
-//       }
-
-// console.log(hamburgerPrice);
-
-
-// var checkboxes = document.querySelectorAll("input[type='checkbox']");
-// console.log(checkboxes);
-// var cheese = document.getElementById("cheese").checked;
-// console.log(cheese);
-
-// var cheesePrice = document.getElementById("cheese").getAttribute("data-price");
-// console.log(cheesePrice);
+//Declaring a variable to store all the discount codes;
+var discountList = ['code003PA', 'code004PA', 'code005PA'];
+console.log(discountList);
 
 
 
-// document.getElementById("calc_btn").addEventListener("click", function calculateHamburgerPrice () {
-//     if (document.getElementById("cheese").checked === true) {
-//       hamburgerPrice = hamburgerPrice + toppingsWithPrice[0][1];   
-//       console.log(hamburgerPrice);
-//       console.log(cheese);
-//     }
-//     console.log(hamburgerPrice);
-//   }
-
-// );
-
-
-function attempt(array, element) {
-  /*for (var i=0; i<array.length; i++) {
+function autotoppings(array, element) {
+  for (var i=0; i<3; i++) {
 
     element.insertAdjacentHTML('afterbegin',
     `
+    <div class="d_flex1">
     <div class="form-toppings">
+    <img src="./img/${toppingsWithPrice[i][0]}.svg"></img>
     <label for="${toppingsWithPrice[i][0]}">${toppingsWithPrice[i][0]}</label>
     <input type="checkbox" name="${toppingsWithPrice[i][0]}" id="${toppingsWithPrice[i][0]}" data-price="${toppingsWithPrice[i][1]}">
+    </div>
+    </div> 
+    `
+    );
+
+
+  }
+  for (var i=3; i<6; i++) {
+
+    element.insertAdjacentHTML('afterbegin',
+    `
+    <div class="d_flex2">
+    <div class="form-toppings">
+    <img src="./img/${toppingsWithPrice[i][0]}.svg"></img>
+    <label for="${toppingsWithPrice[i][0]}">${toppingsWithPrice[i][0]}</label>
+    <input type="checkbox" name="${toppingsWithPrice[i][0]}" id="${toppingsWithPrice[i][0]}" data-price="${toppingsWithPrice[i][1]}">
+    </div>
     </div>
     `
     );
 
 
   }
-}*/
-
-var hamburgerPrice = 9.50;
-
-var toppingsWithPrice = [["Cheese", 1.0], ["Egg", 1.5], ["Mustard", 0.5], ["Tomato", 1.0], ["Lettuce", 1.0], ["Ketchup", 0.5]];
-
+}
 
 
 var toppingsElement = document.querySelector('.toppings');
+//calling the function to automatically generate my toppings code in HTML from the Array toppingsWithPrice 
+// autotoppings(toppingsWithPrice, toppingsElement);
 
-attempt(toppingsWithPrice, toppingsElement);
+
+
+
+//creating a button that starts my program calculating the price of the Burger
+document.getElementById('calc_btn').addEventListener('click', function () {
+var hamburgerPrice = 9.50;
+console.log(hamburgerPrice);
+
+    //saving in a variable the checkes with a querySelector that automatically checks the one that are selected by the user
+    var checkes = document.querySelectorAll("input[type='checkbox']");
+    console.log(checkes);
+
+    var toppingsSum = null;
+    //console.log(toppingsSum);
+
+    for (var j=0; j<checkes.length; j++){
+        var topping = checkes[j];
+        if(topping.checked){
+            toppingsSum = toppingsSum + Number(topping.getAttribute('data-price'));
+        }
+    }
+    //checking the total price of the toppings selected, to be added to the burger price;
+    console.log(toppingsSum);
+
+    var totalBurgerPrice = hamburgerPrice + toppingsSum;
+    console.log(totalBurgerPrice);
+
+    document.getElementById('totalPrice').innerHTML= totalBurgerPrice + " €";
+
+    });
